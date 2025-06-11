9dbd9af34cddbbddc6d5331f698c7f0b9fc7ff53
jamespetran <74488608+jamespetran@users.noreply.github.com>
2025-06-10 21:22:06 -0500

    refinement

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -3,6 +3,36 @@ alias ls='eza --icons'
 alias cat='bat'
 alias find='fd'
 
+# Git
+alias g='git'
+alias gst='git status'
+alias gcm='git commit -m'
+alias gaa='git add .'
+alias gb='git branch'
+alias gco='git checkout'
+alias glg='git log --oneline --decorate --graph --all'
+alias gcl='git clone'
+
 # zoxide (better cd)
 eval "$(zoxide init zsh)"
 
+# App launchers
+alias idea='flatpak run com.jetbrains.IntelliJ-IDEA-Ultimate'
+
+# Docker/Podman smart aliasing
+if command -v podman &> /dev/null && grep -q silverblue /etc/os-release 2>/dev/null; then
+  alias d='podman'
+  alias dps='podman ps'
+  alias dcu='podman-compose up'
+  alias dcd='podman-compose down'
+else
+  alias d='docker'
+  alias dps='docker ps'
+  alias dcu='docker compose up'
+  alias dcd='docker compose down'
+fi
+
+# Navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ~='cd ~'
